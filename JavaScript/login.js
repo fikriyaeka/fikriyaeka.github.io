00efa424d8b877d0eca8a6fcecb90609c712fce9
Fikriya Eka Wahyuni <55588661+fikriyaeka@users.noreply.github.com>
2021-01-13 23:25:58 +0700

    window.location

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -6,7 +6,7 @@ username=username.toLowerCase();
 var password=document.login.password.value;
 password=password.toLowerCase();
 if (username=="pweb" && password=="uasku") { alert("Selamat Login Anda Berhasil "); 
-  window.location = "../Admin/tambah.html" }
+  window.location = "../Admin/pages.html" }
 else 
 if (done==0) { alert(" Login Anda Gagal!"); }
 }
