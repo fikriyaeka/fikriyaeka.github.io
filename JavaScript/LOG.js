828f44074ae32fc3e252a5ddd8ee18cd5580c381
fikriyaeka <fikriyaeka@gmail.com>
2021-01-15 10:00:29 +0700

    Perbaikan Responsive

--- JavaScript/LOG.js
+++ JavaScript/LOG.js
@@ -8,7 +8,7 @@ var pass1=document.log.pass.value;
 if (email1=="" && pass1 == "" ) 
 {alert(" Masukkan Username dan Password dengan Benar ");}
 else
-{ alert("Selamat Sign Up  Anda Berhasil "); 
+{ alert("Selamat LOGIN Anda Berhasil "); 
   window.location = "../Home1/index.html" } 
 
 
